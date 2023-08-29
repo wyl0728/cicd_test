93eb0a753e52c775a2d6be1802d5be07602f534e
wyl0728 <125393986+wyl0728@users.noreply.github.com>
2023-08-29 22:05:24 +0800

    1
    1

---
+++ main.tf
@@ -0,0 +1,32 @@
+provider "google" {
+  project = "sg-rd-ce-yilin-wang"
+  region  = "asia-southeast1"
+}
+
+resource "google_container_cluster" "my_cluster" {
+  name     = "my-gke-cluster"
+  location = "asia-southeast1-b"
+  
+  network    = "mynetwork"
+  subnetwork = "mynetwork"
+
+  # Node pool configuration
+  remove_default_node_pool = true
+
+  node_pool {
+    name               = "default-pool"
+    initial_node_count = 1
+    
+    node_config {
+      machine_type = "e2-small"
+    }
+  }
+}
+
+output "cluster_name" {
+  value = google_container_cluster.my_cluster.name
+}
+
+output "cluster_endpoint" {
+  value = google_container_cluster.my_cluster.endpoint
+}
